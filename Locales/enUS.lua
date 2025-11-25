9fd5200d83cb49e75e023d7346e17775ae47b6a3
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2025-11-25 17:09:43 +0100

    add missing localisations

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -4,6 +4,15 @@ local L = AceLocale:NewLocale(appName, "enUS", true)
 
 if L then
     L["AccessOptionsMessage"] = "Access the options via /at"
-   
+    L["TimelineNotEnabledMessage"] = "The timeline feature is not enabled."
+    L["TimelineNotSupportedMessage"] = "The timeline feature is not supported on this version."
+
+    --options
+    L['addonOptions'] = "Ability Timeline Options"
+    L['encounterOptions'] = "Encounter Options"
+    L['EditTimingsForEncounter'] = "Edit Timings for Encounter"
+    L['TimingsEditorTitle'] = "Timings Editor for: "
+    L['debugMode'] = "Debug Mode"
+    L['debugModeDescription'] = "Enables debug mode, which outputs additional information to the chat window."
     private.localisation = L
 end
