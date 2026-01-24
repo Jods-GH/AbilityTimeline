fea09e2b9b690a630eee80407df4d83c393c21db
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2026-01-24 22:12:25 +0100

    only clear dnd message when one was active

--- Main.lua
+++ Main.lua
@@ -148,6 +148,7 @@ function AbilityTimeline:ENCOUNTER_START(event, encounterID, encounterName, diff
     end
     if not C_ChatInfo.InChatMessagingLockdown() and private.db.profile.enableDNDMessage then
         local name, groupType, isHeroic, isChallengeMode, displayHeroic, displayMythic, toggleDifficultyID, isLFR, minPlayers, maxPlayers = GetDifficultyInfo(difficultyID)
+        private.db.global.active = true
         C_ChatInfo.SendChatMessage(private.getLocalisation("CurrentlyBusyInEncounter"):format(encounterName, name), "DND") 
     end
 
@@ -163,7 +164,10 @@ function AbilityTimeline:ENCOUNTER_END(event, encounterID, encounterName, diffic
     end
 
     if not C_ChatInfo.InChatMessagingLockdown() and private.db.profile.enableDNDMessage then
-        C_ChatInfo.SendChatMessage("", "DND") -- clear dnd message
+        if private.db.global.active then
+            C_ChatInfo.SendChatMessage("", "DND") -- clear dnd message
+            private.db.global.active = false
+        end
     end
 end
 
@@ -264,8 +268,9 @@ function AbilityTimeline:CANCEL_PLAYER_COUNTDOWN()
 end
 
 function AbilityTimeline:CHALLENGE_MODE_RESET(event, mapID)
-    if not C_ChatInfo.InChatMessagingLockdown() and private.db.profile.enableDNDMessage then
+    if not C_ChatInfo.InChatMessagingLockdown() and private.db.profile.enableDNDMessage and private.db.global.active then
         C_ChatInfo.SendChatMessage(private.getLocalisation("CurrentlyDoingMplusKeyFallback"), "DND") 
+        private.db.global.active = false
     end
 end
 
@@ -285,6 +290,7 @@ function AbilityTimeline:CHALLENGE_MODE_START()
             end
         end
         C_ChatInfo.SendChatMessage(message, "DND") 
+        private.db.global.active = true
     end
 end
 
@@ -308,7 +314,7 @@ function AbilityTimeline:CHALLENGE_MODE_COMPLETED()
         private.Debug("Challenge mode completed Adding timer for key reroll: 5 minutes.")
         private.RerollKeyEventId = C_EncounterTimeline.AddScriptEvent(eventinfo)
     end
-    if private.db.profile.enableDNDMessage then
+    if private.db.profile.enableDNDMessage and private.db.global.active then
         C_ChatInfo.SendChatMessage("", "DND") 
     end
 end
@@ -318,7 +324,7 @@ function AbilityTimeline:ZONE_CHANGED_NEW_AREA()
         C_EncounterTimeline.CancelScriptEvent(private.RerollKeyEventId)
         private.RerollKeyEventId = nil
     end
-    if private.db.profile.enableDNDMessage then
+    if private.db.profile.enableDNDMessage and private.db.global.active then
         C_ChatInfo.SendChatMessage("", "DND") 
     end
 end
