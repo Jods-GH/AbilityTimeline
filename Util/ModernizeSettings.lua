3b3dc651d49fb999e61b6a7a9d01160dd9745372
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2026-03-25 20:28:25 +0100

    properly initialize font settings for bigicon

--- Util/ModernizeSettings.lua
+++ Util/ModernizeSettings.lua
@@ -151,6 +151,11 @@ private.modernize = function()
     if not private.db.profile.big_icon_text_settings.font then
         private.db.profile.big_icon_text_settings.font = "Friz Quadrata TT"
     end
+    if not private.db.profile.big_icon_text_settings.fontFlag then
+        private.db.profile.big_icon_text_settings.fontFlag = {
+            OUTLINE = true,
+        }
+    end
     if private.db.profile.big_icon_text_settings.useBackground == nil then
         private.db.profile.big_icon_text_settings.useBackground = false
     end
