b7573b0c270fc4267b45cf5d7d2291545c7402c0
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2026-07-01 10:03:47 +0000

    chore: update encounter data

--- Data/seasons.lua
+++ Data/seasons.lua
@@ -2,11 +2,19 @@
 local addonName, private = ...
 private.Seasons = private.Seasons or {}
 
+private.Seasons["season-mn-2"] = { 
+  name = "MN Season 2", 
+  short_name = "MN2", 
+  starts = { us = 1797433200, eu = 1797480000, tw = 1797548400, kr = 1797548400, cn = 1797548400 }, 
+  ends = { us = 1893456000, eu = 1893456000, tw = 1893456000, kr = 1893456000, cn = 1893456000 }, 
+  dungeons = { 1030, 1041, 1202 } 
+}
+
 private.Seasons["season-mn-1"] = { 
   name = "MN Season 1", 
   short_name = "MN1", 
   starts = { us = 1774364400, eu = 1774411200, tw = 1774479600, kr = 1774479600, cn = 1774479600 }, 
-  ends = { us = 1893456000, eu = 1893456000, tw = 1893456000, kr = 1893456000, cn = 1893456000 }, 
+  ends = { us = 1797433200, eu = 1797480000, tw = 1797548400, kr = 1797548400, cn = 1797548400 }, 
   dungeons = { 278, 476, 945, 1201, 1299, 1300, 1315, 1316 } 
 }
 
