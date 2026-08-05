73ac2e23cd980744c0ac3d23d4f91b8e6cf16a19
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2026-08-05 08:46:42 +0000

    chore: update encounter data

--- Data/seasons.lua
+++ Data/seasons.lua
@@ -5,16 +5,32 @@ private.Seasons = private.Seasons or {}
 private.Seasons["season-mn-2"] = { 
   name = "MN Season 2", 
   short_name = "MN2", 
-  starts = { us = 1797433200, eu = 1797480000, tw = 1797548400, kr = 1797548400, cn = 1797548400 }, 
+  starts = { us = 1787065200, eu = 1787112000, tw = 1787180400, kr = 1787180400, cn = 1787180400 }, 
   ends = { us = 1893456000, eu = 1893456000, tw = 1893456000, kr = 1893456000, cn = 1893456000 }, 
   dungeons = { 1030, 1041, 1202 } 
 }
 
 private.Seasons["season-mn-1"] = { 
+  name = "MN Season 1 • Full", 
+  short_name = "MN1 (Full)", 
+  starts = { us = 1774364400, eu = 1774411200, tw = 1774479600, kr = 1774479600, cn = 1774479600 }, 
+  ends = { us = 1787065200, eu = 1787112000, tw = 1787180400, kr = 1787180400, cn = 1787180400 }, 
+  dungeons = { 278, 476, 945, 1201, 1299, 1300, 1315, 1316 } 
+}
+
+private.Seasons["season-mn-1-cutoffs"] = { 
   name = "MN Season 1", 
   short_name = "MN1", 
   starts = { us = 1774364400, eu = 1774411200, tw = 1774479600, kr = 1774479600, cn = 1774479600 }, 
-  ends = { us = 1797433200, eu = 1797480000, tw = 1797548400, kr = 1797548400, cn = 1797548400 }, 
+  ends = { us = 1786460400, eu = 1786507200, tw = 1786575600, kr = 1786575600, cn = 1786575600 }, 
+  dungeons = { 278, 476, 945, 1201, 1299, 1300, 1315, 1316 } 
+}
+
+private.Seasons["season-mn-1-break-the-meta"] = { 
+  name = "Midnight Season 1 • Break the Meta", 
+  short_name = "BTM MN1", 
+  starts = { us = 1784041200, eu = 1784041200, tw = 1784041200, kr = 1784041200, cn = 1784156400 }, 
+  ends = { us = 1784646000, eu = 1784646000, tw = 1784646000, kr = 1784646000, cn = 1784761200 }, 
   dungeons = { 278, 476, 945, 1201, 1299, 1300, 1315, 1316 } 
 }
 
